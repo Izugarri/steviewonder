4597165c953d395afdb20ed3c081156b4e85cebe
izugarri3 <videomusicajmc@gmail.com>
2026-03-08 15:35:24 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,44 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
+Deno.serve(async (req) => {
+  const url = new URL(req.url);
+  const pathname = url.pathname;
+
+  // 2. Regla: CSS/JS (Caché media: 1 hora)
+  if (pathname.endsWith("Control.html") || pathname.endsWith(".js")) {
+    const res = await serveDir(req, {
+      fsRoot: "./",
+    });
+    res.headers.set("Cache-Control", "public, max-age=5", "Deno-CDN-Cache-Control", "public, s-maxage=6");
+    return res;
+  }
+
+  // 3. Regla por defecto: HTML (No caché, verificar siempre)
+  const res = await serveDir(req, {
+    fsRoot: "public",
+    showIndex: true, // Sirve index.html automáticamente
+  });
+  res.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  return res;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { serveDir } from "jsr:@std/http/file-server";
+
 Deno.serve((req) => {
   const pathname = new URL(req.url).pathname;
 
