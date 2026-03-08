2393f6cf65c2d439c9b3e724623843fd6c76ca83
izugarri3 <videomusicajmc@gmail.com>
2026-03-08 16:31:08 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,4 @@ Deno.serve((req) => {
     fsRoot: "./", // Carpeta que contiene los archivos
     showDirListing: true,
   });
-// 1. Regla: Imágenes/Activos inmutables (Caché larga: 1 año)
-  if (pathname.startsWith("/Control/")) {
-    const res = await serveDir(req, {
-      fsRoot: "Control",
-      urlRoot: "Control",
-    });
-    res.headers.set("Cache-Control", "public, max-age=7");
-    return res;
-  }
 });
