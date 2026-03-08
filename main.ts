873e17f33b47146bb570712c2beb93b5bb6e56dd
izugarri3 <videomusicajmc@gmail.com>
2026-03-08 16:23:34 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,47 @@ Deno.serve((req) => {
     fsRoot: "./", // Carpeta que contiene los archivos
     showDirListing: true,
   });
+;
+
+  // 1. Regla: Imágenes/Activos inmutables (Caché larga: 1 año)
+  if (pathname.startsWith("/Control/")) {
+    const res = await serveDir(req, {
+      fsRoot: "Control",
+      urlRoot: "Control",
+    });
+    res.headers.set("Cache-Control", "public, max-age=3");
+    return res;
+  }
+
+  // 2. Regla: CSS/JS (Caché media: 1 hora)
+  if (pathname.endsWith(".css") || pathname.endsWith(".js")) {
+    const res = await serveDir(req, {
+      fsRoot: "./",
+    });
+    res.headers.set("Cache-Control", "public, max-age=60");
+    return res;
+  }
+
+  // 3. Regla por defecto: HTML (No caché, verificar siempre)
+  const res = await serveDir(req, {
+    fsRoot: "./",
+    showIndex: true, // Sirve index.html automáticamente
+  });
+  res.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  return res;
+});
+
+
+
+
+
+import { serveDir } from "jsr:@std/http/file-server";
+
+Deno.serve((req) => {
+  return serveDir(req, {
+    fsRoot: "./", // Carpeta que contiene los archivos
+    showDirListing: true,
+  });
 }
   // 1. Regla: Imágenes/Activos inmutables (Caché larga: 1 año)
   if (pathname.startsWith("/Control/")) {
