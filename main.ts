a8e6252548ce7cd44f1b987758a374640c6f8113
izugarri3 <videomusicajmc@gmail.com>
2026-03-08 17:07:33 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,12 +5,12 @@ Deno.serve(async (req) => {
   const pathname = url.pathname;
 
   // 1. Regla: Imágenes/Activos inmutables (Caché larga: 1 año)
-  if (pathname.startsWith("/Control/")) {
+  if (pathname.startsWith("Control*")) {
     const res = await serveDir(req, {
-      fsRoot: "Control",
-      urlRoot: "Control",
+      fsRoot: "./",
+      urlRoot: "./",
     });
-    res.headers.set("Deno-CDN-Cache-Control", "public, max-age=2");
+    res.headers.set("Deno-CDN-Cache-Control", "public, no-cache");
     return res;
   }
 
